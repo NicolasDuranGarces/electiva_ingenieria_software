3059edcfdd169fdd9afd44216b2b8d908c6ebec6
Nicolas Duran Garces <nicolas.duran@atlanticsoft.us>
2021-03-02 13:54:24 -0500

    Estructura del Proyecto en Spring Boot 2.4.3 , Maven 11 y Java 11

---
+++ src/main/java/com/eam/ElectiveSoftwareEngineeringApplication.java
@@ -0,0 +1,13 @@
+package com.eam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectiveSoftwareEngineeringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectiveSoftwareEngineeringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eam/ElectiveSoftwareEngineeringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectiveSoftwareEngineeringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
